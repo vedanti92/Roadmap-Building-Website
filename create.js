8e6210b033869fe1d7471b06f7baa19640be90fa
vedanti <vedanti.r.bagade@gmail.com>
2024-01-10 20:47:59 +0530

    file updated

--- create.js
+++ create.js
@@ -1,3 +1,10 @@
 db.inventory.insertOne(
     { item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } }
- )
+)
+
+db.inventory.insertMany(
+    [{ item: "canvas1", qty: 50, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } },
+    { item: "canvas2", qty: 90, tags: ["copper"], size: { h: 2, w: 5.5, uom: "cm" } },
+    { item: "canvas3", qty: 20, tags: ["zinc"], size: { h: 28, w: 35, uom: "cm" } },
+    { item: "canvas4", qty: 80, tags: ["aluminium"], size: { h: 8, w: 35.5, uom: "cm" } }]
+)
