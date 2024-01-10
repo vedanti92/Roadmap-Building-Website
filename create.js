d10d168977b2bb74005509284b20869a486ded52
vedanti <vedanti.r.bagade@gmail.com>
2024-01-10 20:49:13 +0530

    file updated

--- create.js
+++ create.js
@@ -8,3 +8,9 @@ db.inventory.insertMany(
     { item: "canvas3", qty: 20, tags: ["zinc"], size: { h: 28, w: 35, uom: "cm" } },
     { item: "canvas4", qty: 80, tags: ["aluminium"], size: { h: 8, w: 35.5, uom: "cm" } }]
 )
+
+db.inventory.insertMany([
+    { item: "journal", qty: 25, tags: ["blank", "red"], size: { h: 14, w: 21, uom: "cm" } },
+    { item: "mat", qty: 85, tags: ["gray"], size: { h: 27.9, w: 35.5, uom: "cm" } },
+    { item: "mousepad", qty: 25, tags: ["gel", "blue"], size: { h: 19, w: 22.85, uom: "cm" } }
+ ])
