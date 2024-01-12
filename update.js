3de85dae49220c5aa73b58e2d02bb90b2734d1f2
vedanti <vedanti.r.bagade@gmail.com>
2024-01-12 22:23:48 +0530

    file updated

--- update.js
+++ update.js
@@ -5,3 +5,11 @@ db.inventory.updateOne(
       $currentDate: { lastModified: true }
     }
  )
+
+ db.inventory.updateMany(
+    { "qty": { $lt: 50 } },
+    {
+      $set: { "size.uom": "in", status: "P" },
+      $currentDate: { lastModified: true }
+    }
+ )
