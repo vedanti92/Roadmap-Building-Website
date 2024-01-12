6ffd312b860ea2b29c77cc0ccdc8b6bd66fa1191
vedanti <vedanti.r.bagade@gmail.com>
2024-01-12 22:03:02 +0530

    file updated

--- read.js
+++ read.js
@@ -3,3 +3,5 @@ db.inventory.find({qty:90})
 db.inventory.find({tags:{$in:["red", "D"]}})
 
 db.inventory.find( { status: { $in: [ "A", "D" ] } } )
+
+db.inventory.find( { status: "A", qty: { $lt: 30 } } )
