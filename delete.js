b92d90a3e48a49072e0e0a9d34508bc8ac1a33ad
vedanti <vedanti.r.bagade@gmail.com>
2024-01-16 18:48:02 +0530

    file updated

--- delete.js
+++ delete.js
@@ -3,13 +3,3 @@ db.inventory.deleteMany({})
 db.inventory.deleteOne( { status: "D" } )
 
 db.inventory.deleteMany({ status : "A" })
-
-//limit
-db.inventory.find().limit(1)
-
-//skip
-db.inventory.find().skip(1)
-
-//sort
-db.inventory.find().sort({qty: 1}) //ascending
-db.inventory.find().sort({qty: -1}) //descending
