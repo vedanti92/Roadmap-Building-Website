14672eed0db7f3e6186c05281935771028191721
vedanti <vedanti.r.bagade@gmail.com>
2024-01-16 18:42:12 +0530

    file updated

---
+++ sort.js
@@ -0,0 +1,12 @@
+// Building the Database
+db.inventory.insertMany( [
+    { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+    { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "P" },
+    { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
+    { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
+    { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" },
+ ] );
+
+//sort
+db.inventory.find().sort({qty: 1}) //ascending
+db.inventory.find().sort({qty: -1}) //descending
